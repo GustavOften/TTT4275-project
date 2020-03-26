993ba20ff139923c0ca2e238ede23325d9a50d4e
Gustav <g.often94@gmail.com>
2020-03-26 12:08:02 +0100

    added linux

--- linear_classifier.py
+++ linear_classifier.py
@@ -2,6 +2,12 @@ import numpy as np
 from numpy import genfromtxt
 import matplotlib.pyplot as plt
 
+#Linux
+class_1 = genfromtxt('./TTT4275-project/task_1/Iris_TTT4275/class_1', delimiter=',')
+class_2 = genfromtxt('./TTT4275-project/task_1/Iris_TTT4275/class_2', delimiter=',')
+class_3 = genfromtxt('./TTT4275-project/task_1/Iris_TTT4275/class_3', delimiter=',')
+
+#Windows
 class_1 = genfromtxt('..\TTT4275-project\\task_1\Iris_TTT4275\class_1', delimiter=',')
 class_2 = genfromtxt('..\TTT4275-project\\task_1\Iris_TTT4275\class_2', delimiter=',')
 class_3 = genfromtxt('..\TTT4275-project\\task_1\Iris_TTT4275\class_3', delimiter=',')
