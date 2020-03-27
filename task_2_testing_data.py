88fae48866c3279f52be2c23f192401cded91156
Gustav <g.often94@gmail.com>
2020-03-27 12:32:09 +0100

    importing data as pandas DataFrame

--- task_2_testing_data.py
+++ task_2_testing_data.py
@@ -4,11 +4,8 @@ from numpy import genfromtxt
 import platform
 
 if(platform.system() == 'Windows'): #Windows
-    data = pd.read_csv('..\TTT4275-project\\task_2\Wovels\\vowdata.dat', sep="\s+", header=24)
+    data = pd.read_csv('..\TTT4275-project\\task_2\Wovels\\vowdata.dat', sep="\s+", header=24, index_col=0)
+    data = data.transpose()
 else:  #Linux
-    data = pd.read_csv('task_2/Wovels/vowdata.dat', sep="\s+", header=24)
-
-
-
-
-print(data['1'])
+    data = pd.read_csv('task_2/Wovels/vowdata.dat', sep="\s+", header=24, index_col=0)
+    data = data.transpose()
