ec8fb0305bec2cb6225b75dcc00fcd4505cfcfbd
Nikolai M <nikolaisin@gmail.com>
2020-03-27 12:25:19 +0100

    repaired path to work on linux

--- task_2_testing_data.py
+++ task_2_testing_data.py
@@ -1,6 +1,14 @@
 import numpy as np
 import pandas as pd
 from numpy import genfromtxt
+import platform
 
-data = pd.read_csv('..\TTT4275-project\\task_2\Wovels\\vowdata.dat', sep="\s+", header=24)
+if(platform.system() == 'Windows'): #Windows
+    data = pd.read_csv('..\TTT4275-project\\task_2\Wovels\\vowdata.dat', sep="\s+", header=24)
+else:  #Linux
+    data = pd.read_csv('task_2/Wovels/vowdata.dat', sep="\s+", header=24)
+
+
+
+
 print(data['1'])
